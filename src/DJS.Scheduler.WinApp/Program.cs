99b1b1acbebc962384d00e8adee4061d793ca69f
gongthub <1056685839@qq.com>
2018-03-01 11:02:54 +0800

    准备环境

---
+++ src/DJS.Scheduler.WinApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DJS.Scheduler.WinApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
